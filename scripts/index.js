57b3b694791981cb86f42e67157f553dfdbafe20
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-02-28 11:47:50 -0300

    ajustei erro, agora esta correto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let nome = prompt('Digite seu nome');
 alert('Olá ' + nome + ', vamos calcular seu índice de massa corporal?');
 
+//aqui o usuário deve colocar os valores com ponto e não com vírgula, para o resultado ser correto
 let peso = parseFloat(prompt('Digite seu peso (em kg)'));
 let altura = parseFloat(prompt('Digite sua altura (em metros)'));
 
@@ -9,4 +10,4 @@ let imc = peso / (altura * altura);
 
 alert('Seu IMC é: ' + imc);
 
-console.log("IMC:", imc);
+console.log("IMC:", imc)
