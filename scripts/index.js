0097759b51fd7dbdd367caa020dfa29eb225d052
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-02-28 11:48:30 -0300

    finalizei o desafio

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ alert('Olá ' + nome + ', vamos calcular seu índice de massa corporal?');
 let peso = parseFloat(prompt('Digite seu peso (em kg)'));
 let altura = parseFloat(prompt('Digite sua altura (em metros)'));
 
-// Calculando o IMC
+// calculando o IMC
 let imc = peso / (altura * altura);
 
 alert('Seu IMC é: ' + imc);
