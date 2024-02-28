75ad56dfec5097fe8592c72bcae1375e8a9e21c0
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-02-28 11:25:22 -0300

    criei a função

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+let nome = prompt('Digite seu nome')
+alert('Olá ' + nome + ', vamos calcular seu indice de massa corporal?')
+
+let valor1 = parseInt(prompt('Digite seu peso (em kg)'))
+let valor2 = parseInt(prompt('Digite sua altura (metros)'))
+
+let valor = valor1 / valor2 * valor2
+
+alert('O seu IMC é = ' + valor)
+
